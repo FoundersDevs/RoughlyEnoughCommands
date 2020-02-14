65ba6c5fad54dbf3127a2d42ad9e0affbd466181
shupik <edo4eddie@gmail.com>
2020-02-13 20:47:58 -0500

    created workspace

---
+++ src/main/java/gg/founders/seed_tools/SeedTools.java
@@ -0,0 +1,13 @@
+package gg.founders.seed_tools;
+
+import net.fabricmc.api.ModInitializer;
+
+public class SeedTools implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+        // TODO Auto-generated method stub
+
+    }
+    
+}
