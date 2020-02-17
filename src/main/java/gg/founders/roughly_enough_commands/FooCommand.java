2a8ae9ac6f6132ae2a1b29311a13072f03d491e5
shupik <edo4eddie@gmail.com>
2020-02-17 11:02:35 -0500

    added arg to /foo

--- src/main/java/gg/founders/roughly_enough_commands/FooCommand.java
+++ src/main/java/gg/founders/roughly_enough_commands/FooCommand.java
@@ -1,17 +1,23 @@
 package gg.founders.roughly_enough_commands;
 
 import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.arguments.StringArgumentType;
+
 import io.github.cottonmc.clientcommands.*;
+
 import net.minecraft.text.LiteralText;
 
 public class FooCommand implements ClientCommandPlugin {
     @Override
     public void registerCommands(CommandDispatcher<CottonClientCommandSource> dispatcher) {
-        dispatcher.register(ArgumentBuilders.literal("foo").executes(
-            source -> {
-                source.getSource().sendFeedback(new LiteralText("bar"));
-                return 1;
-            }
-        ));
+        dispatcher
+        .register(ArgumentBuilders.literal("foo")
+        .then(ArgumentBuilders.argument("arg", StringArgumentType.greedyString())
+        .executes(context -> {
+            String message = StringArgumentType.getString(context, "arg");
+            context.getSource().sendFeedback(new LiteralText(message));
+            return 1;
+        }
+        )));
     }
 }
